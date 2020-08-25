47e37675eaa5d983e6b26d3360103845ca089460
ahmed <ahmed@server.example.com>
2020-08-25 13:42:39 +0300

    TAHA

--- models/models.py
+++ models/models.py
@@ -7,8 +7,8 @@ from odoo import models, fields, api
 class Shifaa(models.Model):
     _inherit = "res.partner"
     
-    CATEGORIES=[("bureau","Bureau"),("company","Company"),("drugstore","Drugstore"),("pharmacy","Pharmacy"),("medical supplies","Medical Supplies"),("hospital","Hospital"),("private","Private")]
-    category = fields.Selection(CATEGORIES,string="Category")
+    #CATEGORIES=[("bureau","Bureau"),("company","Company"),("drugstore","Drugstore"),("pharmacy","Pharmacy"),("medical supplies","Medical Supplies"),("hospital","Hospital"),("private","Private")]
+    category = fields.Char(string="Category")
     department = fields.Char(string="Department")
     price_category = fields.Char(string="Price Category")
     client_code = fields.Integer(string="Client Code")
