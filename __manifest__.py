8cf894258b42add0f27472cca55e457c6ed12be1
M-Saeb <mohammed.saeb98@hotmail.com>
2021-01-13 19:05:10 +0000

    fixed duplication bug

--- __manifest__.py
+++ __manifest__.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Al-Shiffa",
+    'name': "Al-Shifaa",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Al-Shifaa Custom Changes""",
 
     'description': """
-        Long description of module's purpose
-    """,
+        A module for custom changes made for Al-Shifaa By Integrated Path. That includes changes to sale.order, stock.picking & hr.employee
+         models and reports""",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Integerated Path",
+    'website': "https://www.int-path.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','sale', 'hr'],
+    'depends': ['base','stock','sale_management', 'hr_contract', 'mai_sale_order_lot_selection', 'account_accountant'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'report/sale_order.xml',
+        'report/sale_order.xml',
         
         
     ],
