b6677c67059a9569f29407bba42a377d5c94d045
intpath <63392324+intpath@users.noreply.github.com>
2021-01-13 20:35:03 +0300

    Update __manifest__.py
    test

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'report/sale_order.xml',
+        #'report/sale_order.xml',
         
         
     ],
