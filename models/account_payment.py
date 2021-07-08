5c98cc7b447a397618789fbd9764dae3f1c1ae41
Hayder <hadermusc@gmail.com>
2021-07-08 11:31:34 +0000

    Fix res_partner problem

--- models/account_payment.py
+++ models/account_payment.py
@@ -9,5 +9,4 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     salesman_id = fields.Many2one("res.partner", "Salesman")
-    partner_category_id = fields.Many2one(related="partner_id.client_category_id", string="Category")
     district_id = fields.Many2one(related="partner_id.district_id", string="District")
